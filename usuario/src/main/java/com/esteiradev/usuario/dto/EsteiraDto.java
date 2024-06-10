48cd2fff255dda9c86b74830508f540c4aa289f1
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2024-06-10 16:00:02 -0300

    Método getAllEsteiraByUser - Sincrona

---
+++ usuario/src/main/java/com/esteiradev/usuario/dto/EsteiraDto.java
@@ -0,0 +1,12 @@
+package com.esteiradev.usuario.dto;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class EsteiraDto {
+
+    private UUID esteiraId;
+    private String titulo;
+}
