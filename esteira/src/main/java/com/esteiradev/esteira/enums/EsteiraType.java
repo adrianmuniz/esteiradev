d9979ddbaf76d371ad9c2295123858699800f0fb
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2025-11-03 21:57:28 -0300

    Inserindo ordem nas esteiras para o front

--- esteira/src/main/java/com/esteiradev/esteira/enums/EsteiraType.java
+++ esteira/src/main/java/com/esteiradev/esteira/enums/EsteiraType.java
@@ -1,11 +1,20 @@
 package com.esteiradev.esteira.enums;
 
 public enum EsteiraType {
-    BACKLOG,
-    ANALISE,
-    DEV,
-    TESTE,
-    QA,
-    RESOLVIDO,
-    OTHER;
+    BACKLOG(1),
+    ANALISE(2),
+    DEV(3),
+    TESTE(4),
+    QA(5),
+    RESOLVIDO(6),
+    OUTROS(7);
+
+    private final int ordem;
+    EsteiraType(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
 }
